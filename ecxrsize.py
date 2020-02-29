f0729bf7f10c87b81893f92c495cfbb54acae938
Adam Berg <adam@xyzdigital.com>
2020-02-29 11:22:22 -0800

    Collect list of cases for parse_source_folders function

--- ecxrsize.py
+++ ecxrsize.py
@@ -1,5 +1,13 @@
 import os
 import argparse
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Case:
+    id: str
+    dicom_files: List[str]
+    report_file: str
 
 
 def main():
@@ -11,10 +19,29 @@ def main():
         help='folder with cases'
     )
     args = parser.parse_args()
+    cases = parse_source_folders(args.source_folder)
+    print(cases)
 
-    # list of all subdirectories
-    subfolders = next(os.walk(args.source_folder))[1]
-    print(subfolders)
+def parse_source_folders(source_folder: str) -> List[Case]:
+    cases = []
+    case_ids = next(os.walk(source_folder))[1]
+
+    for case_id in case_ids:
+        dicom_files = []
+        report_file = ""
+        case_folder = os.path.join(source_folder, case_id)
+        for _, _, files in os.walk(case_folder):
+            for file in files:
+                full_file_path = os.path.join(case_folder, file)
+                if file.endswith('.txt'):
+                    report_file = full_file_path
+                elif file.endswith('.dicom'):
+                    dicom_files.append(full_file_path)
+
+        cases.append(Case(id=case_id, report_file=report_file, dicom_files=dicom_files))
+
+    return cases
+
 
 if __name__ == '__main__':
     main()
