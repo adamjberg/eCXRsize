b95f15a0bfc77ef1b54495caf740b76c821ec2f8
Adam Berg <adam@xyzdigital.com>
2020-03-07 11:58:56 -0800

    Run Comprehend Medical on reports and save as json

---
+++ README.md
@@ -0,0 +1,21 @@
+# eCXRsize
+
+## DICOM Conversion
+
+### How to Run
+
+    python eCXRsize.py cases --images --width 500 --height 500
+
+## Labels via AWS Comprehend Medical
+
+### Initial AWS Setup
+    pip install awscli  #can add user flag 
+    aws configure
+    AWS Access Key ID [****************ABCD]:[enter your key here]
+    AWS Secret Access Key [****************xyz]:[enter your secret key here]
+    Default region name [us-west-2]:[enter your region here]
+    Default output format [None]:
+
+### How to Run
+
+    python eCXRsize.py cases --labels
--- ecxrsize.py
+++ ecxrsize.py
@@ -1,12 +1,17 @@
 import argparse
 import csv
+import json
 import os
 from dataclasses import dataclass
 from typing import List, Tuple
-import pydicom as dicom
+
+import boto3
+
 import cv2
 import matplotlib.pyplot as plt
 import PIL
+import pydicom as dicom
+
 
 @dataclass
 class Case:
@@ -18,21 +23,22 @@ def main():
     parser = argparse.ArgumentParser(
         description='anonymizes x-ray cases and labels output'
     )
-    parser.add_argument(
-        'source_folder',
-        help='folder with cases'
-    )
-    parser.add_argument(
-        'output_folder',
-        help='folder to output'
-    )
+    parser.add_argument('source_folder', help='folder with cases')
+    parser.add_argument('--output', help='output', default='output')
+
+    parser.add_argument('--comprehend', help='run Comprehend Medical on reports', action='store_true')
+
+    parser.add_argument('--images', type=bool, help='convert dicoms to images', default=False)
     parser.add_argument('--width', type=int, help='output width', default=256)
     parser.add_argument('--height', type=int, help='output height', default=256)
 
     args = parser.parse_args()
     cases = parse_source_folders(args.source_folder)
     for case in cases:
-        convert_dicoms_for_case(case, args)
+        if args.images:
+            convert_dicoms_for_case(case, args)
+        if args.comprehend:
+            detect_entities_for_case(case, args)
 
 def parse_source_folders(source_folder: str) -> List[Case]:
     cases = []
@@ -65,7 +71,7 @@ def convert_dicoms_for_case(case: Case, args):
         convert_dicom(dicom_file, output_path=output_path, output_dimensions=(args.width, args.height))
 
 def get_case_output_directory(case: Case, args):
-    return os.path.join(args.output_folder, case.id)
+    return os.path.join(args.output, case.id)
 
 def convert_dicom(dicom_file: str, output_path: str, output_dimensions: Tuple[int]):
     filename = os.path.basename(dicom_file)
@@ -75,6 +81,17 @@ def convert_dicom(dicom_file: str, output_path: str, output_dimensions: Tuple[in
     full_image_path = os.path.join(output_path, image_path)
     resized_img = cv2.resize(pixel_array_numpy, output_dimensions)
     cv2.imwrite(full_image_path, resized_img)
+
+COMPREHEND_MEDICAL_OUTPUT_FILENAME = 'comprehendmedical.json'
+
+def detect_entities_for_case(case: Case, args):
+    client = boto3.client(service_name='comprehendmedical')
+    result = client.detect_entities(Text= 'cerealx 84 mg daily')
+    output_directory = get_case_output_directory(case, args)
+    filename = os.path.join(output_directory, COMPREHEND_MEDICAL_OUTPUT_FILENAME)
+    file = open(filename, "w")
+    file.write(json.dumps(result, indent=2))
+    file.close()
 
 if __name__ == '__main__':
     main()
